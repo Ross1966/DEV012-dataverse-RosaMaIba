5f8fb7d81a7fdc76a2f1d005c49e492d335ddbdb
Ross1966 <ribanezr66@gmail.com>
2023-10-12 12:15:31 -0600

    Commit 1 Rosa Maria

--- src/view.js
+++ src/view.js
@@ -1,6 +1,27 @@
+// Aquí comienza tu código y puedes retornar lo que tu necesites
 export const renderItems = (data) => {
-  console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+ 
+  const  elemento =  data.map((animal) => 
+    `
+    
+     <li>
+     ${animal.id}
+     <dl itemscope itemtype="Animales">
+     <img src="${animal.imageUrl}" alt=${animal.name} />
+     <dt>Nombre:</dt><dd itemprop =''>${animal.name}</dd>
+     <dt>Descripción:</dt><dd itemprop =''>${animal.shortDescription}</dd>
+     <dt>Peso en Kilos</dt><dd itemprop =''>${animal.pesoenKilos}</dd>
+     </dl>
+     </li>
+   
+     
+   `
+  ).join("");
+
+
+
+  //console.log(data)
+  
+  return `<ul>${elemento}</ul>`;
 };
 
