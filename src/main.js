611665f02367fb5d34b68e97ae7f749b1d787195
ROZZYNUEZ14 <palacinrossy@gmail.com>
2023-10-13 11:02:05 -0500

    Se eliminan ramas de todos los archivos

--- src/main.js
+++ src/main.js
@@ -3,8 +3,3 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
-//console.log(example, renderItems(data), data);
-
-const contenedorPrincipal = document.querySelector("#root");
-contenedorPrincipal.innerHTML = renderItems(data);
-console.log(renderItems(data))
--- src/view.js
+++ src/view.js
@@ -2,26 +2,5 @@ export const renderItems = (data) => {
   //console.log(data)
 
 
-  const elemento = data.forEach( animalData => {
-    const li = document.createElement("li");
-    li.innerHTML = `
-    ${animalData.name}
-  <dl itemscope itemtype="Animales">
-    <img src= ${animalData.imageUrl} alt=${animalData.id} />
-    <dt>Nombre:</dt><dd itemprop="name">${animalData.name}</dd>
-    <dt>Descripción:</dt><dd itemprop="description">${animalData.shortDescription}</dd>
-    <dt>Cuantos años pueden vivir:</dt><dd itemprop="longevidad">${animalData.añosdevida}</dd>
-    <dt>Dieta:</dt><dd itemprop="dieta">${animalData.dieta}</dd>
-    <dt>Peso en kilos:</dt><dd itemprop="peso">${animalData.pesoenkilos}</dd>
-  </dl>
-  `;
-    const ul = document.createElement("ul");
-    ul.appendChild(li)
-  });
-
-
   
-    
-  return elemento;
-};
-
+}
